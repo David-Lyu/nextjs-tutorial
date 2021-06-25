f44b842e9e8666ce3d65c5c076f054dd5a415258
David-Lyu <lyu.david@yahoo.com>
2021-06-25 09:22:45 -0700

    added some custom styling and made header working on the main landing page

--- social-media-next/components/header/header.js
+++ social-media-next/components/header/header.js
@@ -1,9 +1,17 @@
+// import styles from '../../styles/Home.module.css';
+import styles from './header.module.css';
+
 export default function Header(props) {
   const isLoggedIn = props.isLoggedIn;
 
   return (
-    <nav>
-      <h3> HEADER</h3>
+    <nav className={`row ${styles.nav}`}>
+      <h3 className={styles.name}> HEADER</h3>
+      <div className={styles.links}>
+        <p>LINKS</p>
+        <p>LINKS</p>
+        <p>LINKS</p>
+      </div>
     </nav>
   );
 }
