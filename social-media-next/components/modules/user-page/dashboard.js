cf13ce1caa9879dbe3d8beb8c7f4c78b088729a5
David-Lyu <lyu.david@yahoo.com>
2021-08-05 10:41:15 -0700

    changed the naming schema of userprofile components and started working on the dashboard

--- social-media-next/components/modules/user-page/dashboard.js
+++ social-media-next/components/modules/user-page/dashboard.js
@@ -1,2 +1,18 @@
+import { useEffect, useState } from 'react';
+
 //this should be either client side or rendered with server-side
 //might be just a component
+//should get id of user wanting to display
+export default function Dashboard({ user }) {
+  const userId = user.id;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {}, []);
+  return (
+    <div>
+      {!isLoaded && <div>Loading screen</div>}
+      {isLoaded && <div>{post.map()}</div>}
+    </div>
+  );
+}
