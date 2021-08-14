d431f41d924951f0c221db06b25e16b70a9df326
David-Lyu <lyu.david@yahoo.com>
2021-08-14 16:01:37 -0700

    in progress adding animations to caret that opens options in header

--- social-media-next/components/Layout/header/ProfileButton/ProfileButton.js
+++ social-media-next/components/Layout/header/ProfileButton/ProfileButton.js
@@ -5,10 +5,16 @@ import Image from 'next/image';
 import styles from './ProfileButton.module.css';
 
 import ProfileOptions from './ProfileOptions';
-
+//going to add another hidden modal so we can click out and it goes away
 export default function ProfileButton(props) {
   const { userId, profilePic } = props;
   const [showOptions, setShowOptions] = useState('hide');
+  const [rotateCaretClass, setRotateCaretClass] = useState('');
+
+  useEffect(() => {
+    if (showOptions === 'hide') setRotateCaretClass(styles['un-rotate-caret']);
+    if (showOptions === 'show') setRotateCaretClass(styles['rotate-caret']);
+  }, [showOptions]);
 
   function onDisplayClick(e) {
     if (showOptions === 'hide') setShowOptions('show');
@@ -16,16 +22,26 @@ export default function ProfileButton(props) {
   }
 
   return (
-    <div className={styles['profile-parent']}>
-      <div className={styles['profile-icon']} onClick={onDisplayClick}>
-        {profilePic && <Image src={profilePic} alt="profile picture" />}
-        {!profilePic && <GoPerson />}
-        <AiOutlineCaretDown />
+    <>
+      <div className={styles['profile-parent']}>
+        <div className={styles['profile-icon']} onClick={onDisplayClick}>
+          {profilePic && <Image src={profilePic} alt="profile picture" />}
+          {!profilePic && <GoPerson />}
+          <AiOutlineCaretDown className={rotateCaretClass} />
+        </div>
+        <div className={styles['profile-options'] + ' ' + showOptions}>
+          <AiFillCaretUp />
+          <ProfileOptions userId={userId} />
+        </div>
       </div>
-      <div className={styles['profile-options'] + ' ' + showOptions}>
-        <AiFillCaretUp />
-        <ProfileOptions />
-      </div>
-    </div>
+      {showOptions === 'show' && (
+        <div
+          className={styles.overlay}
+          onClick={() => {
+            setShowOptions('hide');
+          }}
+        />
+      )}
+    </>
   );
 }
--- social-media-next/components/Layout/header/ProfileButton/ProfileOptions.js
+++ social-media-next/components/Layout/header/ProfileButton/ProfileOptions.js
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import { signOut } from 'next-auth/client';
 
+import styles from './ProfileOptions.module.css';
+
 export default function ProfileOptions(props) {
   const { userId } = props;
+
   return (
-    <ul>
+    <ul className={styles['options-parent']}>
       <li>
         <Link href={`/profile/${userId}`}>
           <a>My profile</a>
